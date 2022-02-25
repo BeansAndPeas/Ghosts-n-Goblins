6f7183b65847c2a96a3344784e09ef0717a49bd4
Ryan Gibson <72360102+BeansAndPeas@users.noreply.github.com>
2022-02-25 15:25:11 +0000

    Fix some errors

--- Assets/Code/Controller.cs
+++ Assets/Code/Controller.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Debug = System.Diagnostics.Debug;
 
 public class Controller : MonoBehaviour {
     [SerializeField]
@@ -14,56 +15,55 @@ public class Controller : MonoBehaviour {
     [SerializeField]
     private Button attackButton;
 
-    private bool touched = false;
+    private bool touched;
     private Vector2 pointA, pointB;
 
     private SpriteRenderer spriteRenderer;
 
     private void Start() {
-        this.spriteRenderer = player.GetComponent<SpriteRenderer>();
-        this.attackButton.onClick.AddListener(() => StartCoroutine(OnAttack));
+        spriteRenderer = player.GetComponent<SpriteRenderer>();
+        attackButton.onClick.AddListener(() => StartCoroutine(OnAttack()));
     }
 
     private IEnumerator OnAttack() {
-        this.attackButton.SetActive(false);
-        this.spriteRenderer.sprite = this.attackSprite;
+        attackButton.gameObject.SetActive(false);
+        Debug.Assert(spriteRenderer != null, nameof(spriteRenderer) + " != null");
+        spriteRenderer.sprite = attackSprite;
         yield return new WaitForSeconds(1.5f);
-        this.spriteRenderer.sprite = this.normalSprite;
-        this.attackButton.SetActive(true);
+        spriteRenderer.sprite = normalSprite;
+        attackButton.gameObject.SetActive(true);
     }
 
     private void Update() {
         if (Input.GetMouseButtonDown(0)) {
-            this.pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+            pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
 
-            this.innerCircle.transform.position = pointA;
-            this.outerCircle.transform.position = pointA;
-            this.innerCircle.GetComponent<SpriteRenderer>().enabled = true;
-            this.outerCircle.GetComponent<SpriteRenderer>().enabled = true;
+            innerCircle.transform.position = pointA;
+            outerCircle.transform.position = pointA;
+            innerCircle.GetComponent<SpriteRenderer>().enabled = true;
+            outerCircle.GetComponent<SpriteRenderer>().enabled = true;
         }
 
         if (Input.GetMouseButton(0)) {
-            this.touched = true;
-            this.pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+            touched = true;
+            pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
         } else {
-            this.touched = false;
+            touched = false;
         }
     }
 
     private void FixedUpdate() {
-        if (this.touched) {
+        if (touched) {
             Vector2 offset = pointB - pointA;
-            Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
+            var direction = Vector2.ClampMagnitude(offset, 1.0f);
             Move(direction);
 
-            this.innerCircle.transform.position = new Vector2(this.pointA.x + direction.x, this.pointA.y + direction.y);
+            innerCircle.transform.position = new Vector2(pointA.x + direction.x, pointA.y + direction.y);
         } else {
-            this.innerCircle.GetComponent<SpriteRenderer>().enabled = false;
-            this.outerCircle.GetComponent<SpriteRenderer>().enabled = false;
+            innerCircle.GetComponent<SpriteRenderer>().enabled = false;
+            outerCircle.GetComponent<SpriteRenderer>().enabled = false;
         }
     }
 
-    private void Move(Vector2 direction) {
-        player.Translate(direction * speed * Time.deltaTime);
-    }
+    private void Move(Vector2 direction) => player.Translate(direction * (speed * Time.deltaTime));
 }
